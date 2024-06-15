f67426e51d17f9579de24ffb27758315244fc763
Ashanti Carpio <acarpio@umich.edu>
2024-06-15 11:01:05 -0400

    Last Push

--- Lab7.py
+++ Lab7.py
@@ -1,7 +1,6 @@
 # CSC 122
 # Substitution Cipher
-# By <Your Name>
-
+# By <Ashanti Carpio>
 """
 Inputs, Processes, Outputs (IOP)
 -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -10,9 +9,30 @@ Processes:
 Output(s):
 """
 
+
 def main():
-    pass # replace this line with all of your solution code
+    # Prompt user for message to encrypt
+    message = input("Enter the message to be encrypted: ")
+
+    # Encrypt the message
+    encrypted_message = ""
+    for char in message:
+        encrypted_message += chr(ord(char) + 3)
+
+    # Print the encrypted message
+    print(f"Encrypted message = {encrypted_message}\n")
+
+    # Prompt user for cipher to decrypt
+    cipher = input("Enter the cipher to be decrypted: ")
+
+    # Decrypt the cipher
+    decrypted_message = ""
+    for char in cipher:
+        decrypted_message += chr(ord(char) - 3)
+
+    # Print the decrypted message
+    print(f"Decrypted message = {decrypted_message}\n")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
